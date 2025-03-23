2b7de02b1428b7a96bdc9af2eeb64e0f8a46409a
Lam Hung Van <vanlamlc9@gmail.com>
2025-03-23 16:37:42 +0700

    InforModal

--- src/features/GoodReceipt/presentation/InforModal/InforModal.jsx
+++ src/features/GoodReceipt/presentation/InforModal/InforModal.jsx
@@ -1,13 +1,16 @@
 import Modal from 'react-modal';
 import clsx from 'clsx';
 import styles from './InforModal.module.scss';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const InforModal = ({isModalOpen, closeModal}) => {
     return (
         <Modal isOpen = {isModalOpen} onRequestClose={closeModal} className={clsx(styles.modal)} > 
-            <h2>Thông tin thêm</h2>
+            <h1 className={clsx(styles.title)}>Thông tin nhập kho chi tiết</h1>
             {/* Thêm nội dung modal ở đây */}
-            <button onClick={closeModal}>Đóng</button>
+            <button className ={clsx(styles.modalClose)} onClick={closeModal}>
+                <AiOutlineClose style={{fontWeight: "bold"}}/>
+            </button>
         </Modal>
     );
 };
